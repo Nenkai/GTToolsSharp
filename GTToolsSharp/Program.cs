a59e6f17e8bf455bdaab84b88b3d02de3a593fc1
Nenkai <Nenkai@users.noreply.github.com>
2026-09-17 14:30:17 +0200

    bump version

--- GTToolsSharp/Program.cs
+++ GTToolsSharp/Program.cs
@@ -29,7 +29,7 @@ class Program
     public static bool SaveHeader = false;
     public static bool SaveTOC = false;
 
-    public const string Version = "5.3.3";
+    public const string Version = "5.3.4";
 
     static async Task Main(string[] args)
     {
